59f2f87cd25c982c66554a79bec54074e64223bb
Benjamín García Roqués <benjamingarciaroques@gmail.com>
2026-08-11 15:27:15 -0300

    use SDL_Surface for true CPU-only rasterization

--- main.c
+++ main.c
@@ -2,10 +2,13 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
 struct Renderer_State
 {
     SDL_Window* window;
-    SDL_Renderer* renderer;
+    SDL_Surface* pixel_buffer;
 };
 
 SDL_AppResult SDL_AppInit(void** app_state, int argc, char** argv)
@@ -20,9 +23,15 @@ SDL_AppResult SDL_AppInit(void** app_state, int argc, char** argv)
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("3D renderer", 800, 800, 0, &state.window, &state.renderer)) {
-        SDL_Log("Could not create window and/or renderer: %s\n", SDL_GetError());
-        SDL_Quit();
+    state.window = SDL_CreateWindow("3DRenderer", WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+    if (state.window == NULL) {
+        SDL_Log("Could not create window: %s\n", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    state.pixel_buffer = SDL_CreateSurface(WINDOW_WIDTH, WINDOW_HEIGHT, SDL_PIXELFORMAT_ARGB8888);
+    if (state.pixel_buffer == NULL) {
+        SDL_Log("Could not create surface: %s\n", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
@@ -40,7 +49,6 @@ SDL_AppResult SDL_AppEvent(void* app_state, SDL_Event* event)
             if (event->key.key == SDLK_ESCAPE) {
                 return SDL_APP_SUCCESS;
             }
-
             break;
         }
     }
@@ -52,16 +60,34 @@ SDL_AppResult SDL_AppEvent(void* app_state, SDL_Event* event)
 SDL_AppResult SDL_AppIterate(void* app_state)
 {
     struct Renderer_State* state = (struct Renderer_State*) app_state;
-    SDL_SetRenderDrawColor(state->renderer, 0, 0, 0, 255);
-    SDL_RenderClear(state->renderer);
-    SDL_RenderPresent(state->renderer);
+
+    // Clear color/frame buffer with a given color
+    SDL_LockSurface(state->pixel_buffer);
+    uint32_t* pixels = (uint32_t*) state->pixel_buffer->pixels;
+
+    for (uint32_t y = 0; y < WINDOW_HEIGHT; y++) {
+        for (uint32_t x = 0; x < WINDOW_WIDTH; x++) {
+            pixels[(WINDOW_WIDTH * y) + x] = 0xFFFFFF00;
+        }
+    }
+    SDL_UnlockSurface(state->pixel_buffer);
+
+    SDL_Surface* window_surface = SDL_GetWindowSurface(state->window);
+    if (window_surface == NULL) {
+        SDL_Log("Couldn't get window surface from main renderer window: %s\n", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    // Copy surface to the screen
+    SDL_BlitSurfaceScaled(state->pixel_buffer, NULL, window_surface, NULL, SDL_SCALEMODE_NEAREST);
+    SDL_UpdateWindowSurface(state->window);
     return SDL_APP_CONTINUE;
 }
 
 void SDL_AppQuit(void* app_state, SDL_AppResult result)
 {
     struct Renderer_State* state = (struct Renderer_State*) app_state;
-    if (state->renderer) SDL_DestroyRenderer(state->renderer);
+    if (state->pixel_buffer) SDL_DestroySurface(state->pixel_buffer);
     if (state->window) SDL_DestroyWindow(state->window);
     SDL_Quit();
 }
